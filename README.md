f1930e04f1792fdabb068eb65884bb6f52525391
AndrewRPorter <pavelow53.andrew@gmail.com>
2019-05-19 21:29:25 -0400

    Andrew: inital commit of changed fork code

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 RAKE
 ====
 
-A Python implementation of the Rapid Automatic Keyword Extraction (RAKE) algorithm as described in: Rose, S., Engel, D., Cramer, N., & Cowley, W. (2010). Automatic Keyword Extraction from Individual Documents. In M. W. Berry & J. Kogan (Eds.), Text Mining: Theory and Applications: John Wiley & Sons.
+This is a fork of [RAKE](https://github.com/aneesha/RAKE) with a focus on speed and readability.
 
-The source code is released under the MIT License.
+This fork uses a different `StopList` and utilizes a large english word corpus to generate more accurate and useful phrase scores. The corpus can be purchased at [Word frequency data](https://www.wordfrequency.info/free.asp).
+
+Usage
+=====
+
+```python
+r = Rake()
+print(r.get_phrases("Some input text..."))
+print(r.get_phrases("Some input text...", length=5))  # length of phrase list can be explicitly specified
+```
--- rake.py
+++ rake.py
@@ -1,36 +1,17 @@
-# Implementation of RAKE - Rapid Automtic Keyword Exraction algorithm
-# as described in:
-# Rose, S., D. Engel, N. Cramer, and W. Cowley (2010). 
-# Automatic keyword extraction from indi-vidual documents. 
-# In M. W. Berry and J. Kogan (Eds.), Text Mining: Applications and Theory.unknown: John Wiley and Sons, Ltd.
-
-import re
+#!/usr/bin/env python3
+import json
 import operator
+import os
+import re
 
-debug = False
-test = True
+freq_words = "word_freqs.json"
+stop_list = "StopList.txt"
 
 
 def is_number(s):
-    try:
-        float(s) if '.' in s else int(s)
-        return True
-    except ValueError:
-        return False
-
-
-def load_stop_words(stop_word_file):
-    """
-    Utility function to load stop words from a file and return as a list of words
-    @param stop_word_file Path and file name of a file containing stop words.
-    @return list A list of stop words.
-    """
-    stop_words = []
-    for line in open(stop_word_file):
-        if line.strip()[0:1] != "#":
-            for word in line.split():  # in case more than one per line
-                stop_words.append(word)
-    return stop_words
+    """Determins if input string is a number"""
+    s = s.replace("%", "")
+    return str.isdigit(s)
 
 
 def separate_words(text, min_word_return_size):
@@ -39,12 +20,16 @@ def separate_words(text, min_word_return_size):
     @param text The text that must be split in to words.
     @param min_word_return_size The minimum no of characters a word must have to be included.
     """
-    splitter = re.compile('[^a-zA-Z0-9_\\+\\-/]')
+    splitter = re.compile("[^a-zA-Z0-9_\\+\\-/]")
     words = []
     for single_word in splitter.split(text):
         current_word = single_word.strip().lower()
-        #leave numbers in phrase, but don't count as words, since they tend to invalidate scores of their phrases
-        if len(current_word) > min_word_return_size and current_word != '' and not is_number(current_word):
+        # leave numbers in phrase, but don't count as words, since they tend to invalidate scores of their phrases
+        if (
+            len(current_word) > min_word_return_size
+            and current_word != ""
+            and not is_number(current_word)
+        ):
             words.append(current_word)
     return words
 
@@ -52,27 +37,18 @@ def separate_words(text, min_word_return_size):
 def split_sentences(text):
     """
     Utility function to return a list of sentences.
-    @param text The text that must be split in to sentences.
+    @param text The text that must be split into sentences.
     """
-    sentence_delimiters = re.compile(u'[.!?,;:\t\\\\"\\(\\)\\\'\u2019\u2013]|\\s\\-\\s')
+    sentence_delimiters = re.compile(u"[.!?,;:\t\\\\\"\\(\\)\\'\u2019\u2013]|\\s\\-\\s")
     sentences = sentence_delimiters.split(text)
     return sentences
 
 
-def build_stop_word_regex(stop_word_file_path):
-    stop_word_list = load_stop_words(stop_word_file_path)
-    stop_word_regex_list = []
-    for word in stop_word_list:
-        word_regex = r'\b' + word + r'(?![\w-])'  # added look ahead for hyphen
-        stop_word_regex_list.append(word_regex)
-    stop_word_pattern = re.compile('|'.join(stop_word_regex_list), re.IGNORECASE)
-    return stop_word_pattern
-
-
 def generate_candidate_keywords(sentence_list, stopword_pattern):
+    """"""
     phrase_list = []
     for s in sentence_list:
-        tmp = re.sub(stopword_pattern, '|', s.strip())
+        tmp = re.sub(stopword_pattern, "|", s.strip())
         phrases = tmp.split("|")
         for phrase in phrases:
             phrase = phrase.strip().lower()
@@ -82,19 +58,22 @@ def generate_candidate_keywords(sentence_list, stopword_pattern):
 
 
 def calculate_word_scores(phraseList):
+    """"""
     word_frequency = {}
     word_degree = {}
+
     for phrase in phraseList:
         word_list = separate_words(phrase, 0)
         word_list_length = len(word_list)
         word_list_degree = word_list_length - 1
-        #if word_list_degree > 3: word_list_degree = 3 #exp.
+
         for word in word_list:
             word_frequency.setdefault(word, 0)
             word_frequency[word] += 1
             word_degree.setdefault(word, 0)
-            word_degree[word] += word_list_degree  #orig.
-            #word_degree[word] += 1/(word_list_length*1.0) #exp.
+
+            word_degree[word] += word_list_degree  # orig.
+
     for item in word_frequency:
         word_degree[item] = word_degree[item] + word_frequency[item]
 
@@ -102,14 +81,23 @@ def calculate_word_scores(phraseList):
     word_score = {}
     for item in word_frequency:
         word_score.setdefault(item, 0)
-        word_score[item] = word_degree[item] / (word_frequency[item] * 1.0)  #orig.
-    #word_score[item] = word_frequency[item]/(word_degree[item] * 1.0) #exp.
+        word_score[item] = word_degree[item] / (word_frequency[item] * 1.0)  # orig.
+
     return word_score
 
 
 def generate_candidate_keyword_scores(phrase_list, word_score):
+    """"""
     keyword_candidates = {}
     for phrase in phrase_list:
+
+        phrase_words = phrase.split(" ")
+
+        if len(phrase_words) > 2:
+            continue
+        elif len([i for i in phrase_words if is_number(i)]) > 0:
+            continue
+
         keyword_candidates.setdefault(phrase, 0)
         word_list = separate_words(phrase, 0)
         candidate_score = 0
@@ -120,49 +108,72 @@ def generate_candidate_keyword_scores(phrase_list, word_score):
 
 
 class Rake(object):
-    def __init__(self, stop_words_path):
-        self.stop_words_path = stop_words_path
-        self.__stop_words_pattern = build_stop_word_regex(stop_words_path)
+    """Rapid automated keyword extraction implementation
 
-    def run(self, text):
+    Try and store the rake object globally as it will help speed up exection times during
+    initialization
+
+    Attributes:
+        stop_words_pattern (re.Pattern): pattern to match all stop words in input text
+        frequent_words (dict): large english corpus with frequencies from BYU
+    """
+
+    def __init__(self):
+        self.stop_words_pattern = self._build_stop_word_regex()
+        self.frequent_words = self._get_frequent_english_words()
+
+    def _load_stop_words(self):
+        """
+        Utility function to load stop words from a file and return as a list of words
+        @param stop_word_file Path and file name of a file containing stop words.
+        @return list A list of stop words.
+        """
+        with open(stop_list, "r") as f:
+            lines = f.readlines()
+            lines = [line.strip() for line in lines]
+        return lines
+
+    def _build_stop_word_regex(self):
+        """Creates a pattern to match stop words from the stop list"""
+        stop_word_list = self._load_stop_words()
+        stop_word_regex_list = []
+        for word in stop_word_list:
+            word_regex = r"\b" + word + r"(?![\w-])"  # added look ahead for hyphen
+            stop_word_regex_list.append(word_regex)
+        stop_word_pattern = re.compile("|".join(stop_word_regex_list), re.IGNORECASE)
+        return stop_word_pattern
+
+    def _get_frequent_english_words(self):
+        """Returns a dictionary of frequent english words from BYU corpus"""
+        with open(
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), freq_words), "r"
+        ) as f:
+            words = json.load(f)
+        return words
+
+    def get_phrases(self, text, length=None):
+        """
+        Returns a sorted list of phrases
+        @param text: input text to extra phrases from
+        @param length: specify the length of the returned phrase list
+        """
         sentence_list = split_sentences(text)
 
-        phrase_list = generate_candidate_keywords(sentence_list, self.__stop_words_pattern)
+        phrase_list = generate_candidate_keywords(
+            sentence_list, self.stop_words_pattern
+        )
 
         word_scores = calculate_word_scores(phrase_list)
 
         keyword_candidates = generate_candidate_keyword_scores(phrase_list, word_scores)
 
-        sorted_keywords = sorted(keyword_candidates.iteritems(), key=operator.itemgetter(1), reverse=True)
+        sorted_keywords = sorted(
+            keyword_candidates.items(), key=operator.itemgetter(1), reverse=True
+        )
+
+        if length:
+            if length > len(sorted_keywords):
+                return sorted_keywords
+            return sorted_keywords[:10]
+
         return sorted_keywords
-
-
-if test:
-    text = "Compatibility of systems of linear constraints over the set of natural numbers. Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets of solutions for all types of systems are given. These criteria and the corresponding algorithms for constructing a minimal supporting set of solutions can be used in solving all the considered types of systems and systems of mixed types."
-
-    # Split text into sentences
-    sentenceList = split_sentences(text)
-    #stoppath = "FoxStoplist.txt" #Fox stoplist contains "numbers", so it will not find "natural numbers" like in Table 1.1
-    stoppath = "SmartStoplist.txt"  #SMART stoplist misses some of the lower-scoring keywords in Figure 1.5, which means that the top 1/3 cuts off one of the 4.0 score words in Table 1.1
-    stopwordpattern = build_stop_word_regex(stoppath)
-
-    # generate candidate keywords
-    phraseList = generate_candidate_keywords(sentenceList, stopwordpattern)
-
-    # calculate individual word scores
-    wordscores = calculate_word_scores(phraseList)
-
-    # generate candidate keyword scores
-    keywordcandidates = generate_candidate_keyword_scores(phraseList, wordscores)
-    if debug: print keywordcandidates
-
-    sortedKeywords = sorted(keywordcandidates.iteritems(), key=operator.itemgetter(1), reverse=True)
-    if debug: print sortedKeywords
-
-    totalKeywords = len(sortedKeywords)
-    if debug: print totalKeywords
-    print sortedKeywords[0:(totalKeywords / 3)]
-
-    rake = Rake("SmartStoplist.txt")
-    keywords = rake.run(text)
-    print keywords
